67f842d48610ac8d0f76c87530123bca0c0a43f4
Kriaaaaa <aleks.nekrasova2015@yandex.ru>
2024-10-29 21:38:59 +0700

    Норм кнопки

--- src/frontend/JS/core.js
+++ src/frontend/JS/core.js
@@ -1,6 +1,10 @@
 document.querySelectorAll('.btn').forEach(button => {
     button.addEventListener('click', function() {
-      this.classList.toggle('clicked');
+      // Удаление класса 'clicked' с других кнопок
+      document.querySelectorAll('.btn.clicked').forEach(btn => {
+        btn.classList.remove('clicked');
+      });
+      this.classList.add('clicked'); // Добавление класса 'clicked' к нажатой кнопке
     });
   });
-
+  
