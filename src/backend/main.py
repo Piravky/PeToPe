a31f7fab71d231bb1d0d4b49ef2a86671b00f76d
Mdalia <mdalia@student.21-school.ru>
2024-10-30 00:07:21 +0700

    add models

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
 from routers import users, dog, cat
-# from routers.users import router as users_router
-# from routers.dog import router as dog_router
-# from routers.cat import router as cat_router
 
 app = FastAPI(
     title="PeToPe"
@@ -11,9 +8,6 @@ app = FastAPI(
 app.include_router(users.router)
 app.include_router(dog.router)
 app.include_router(cat.router)
-# app.include_router(users_router)
-# app.include_router(dog_router)
-# app.include_router(cat_router)
 
 
 # if __name__ == "__main__":
---
+++ src/backend/models.py
@@ -0,0 +1,76 @@
+from sqlmodel import SQLModel, Field
+
+
+class User(SQLModel, table=True):
+    __tablename__ = "users"
+
+    id: int | None = Field(default=None, primary_key=True)
+    score_cat: int | None = None
+    score_dog: int | None = None
+    email: str
+    password: str
+    name: str
+
+
+class AnswersDogs(SQLModel, table=True):
+    __tablename__ = "AnswersDogs"
+
+    id: int | None = Field(default=None, primary_key=True)
+    answers: str
+    score: int
+    id_question: int
+
+
+class Dog_breed(SQLModel, table=True):
+    __tablename__ = "dogs"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    description: str
+    activity: str
+    size: str
+    wool: str
+    allergy: str
+    communication: str
+    attentiveness: int
+    health_risks: str
+    scores: int
+
+
+class QuestionDogs(SQLModel, table=True):
+    __tablename__ = "QuestionDogs"
+
+    id_question: int | None = Field(default=None, primary_key=True)
+    question: str
+
+
+class AnswersCats(SQLModel, table=True):
+    __tablename__ = "AnswersCats"
+
+    id: int | None = Field(default=None, primary_key=True)
+    answers: str
+    score: int
+    id_question: int
+
+
+class Cat_breed(SQLModel, table=True):
+    __tablename__ = "cats"
+
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    description: str
+    activity: str
+    size: str
+    wool: str
+    allergy: str
+    communication: str
+    allergy: str
+    care: str
+    scores: int
+
+
+class QuestionCats(SQLModel, table=True):
+    __tablename__ = "QuestionCats"
+
+    id_question: int | None = Field(default=None, primary_key=True)
+    question: str
